a1aec9a530ce6d9afc0efcb6e637cdaa92ec633d
Ivan Labrada <ilabrada@outlook.com>
2025-08-10 00:16:52 -0400

    Big button showing slapsed time

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'dart:async';
 
 void main() {
   runApp(const MyApp());
@@ -10,28 +12,65 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // TRY THIS: Try running your application with "flutter run". You'll see
+          // the application has a purple toolbar. Then, without quitting the app,
+          // try changing the seedColor in the colorScheme below to Colors.green
+          // and then invoke "hot reload" (save your changes or press the "hot
+          // reload" button in a Flutter-supported IDE, or press "r" if you used
+          // the command line to start the app).
+          //
+          // Notice that the counter didn't reset back to zero; the application
+          // state is not lost during the reload. To reset the state, use hot
+          // restart instead.
+          //
+          // This works for code too, not just values: Most code changes can be
+          // tested with just a hot reload.
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        ),
+        home: const MyHomePage(title: 'The Rummikub Timer'),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var current = 1;
+  int secondsElapsed = 0;
+  Timer? _timer;
+  var turnTime = 120;
+  var numPlayers = 4;
+  var playersName = <String>['Player 1', 'Player 2', 'Player 3', 'Player 4'];
+
+  void nextPlayer() {
+    current = (current % numPlayers) + 1;
+    startTimer();
+    notifyListeners();
+  }
+
+  void startTimer() {
+    resetTimer();
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      if (secondsElapsed < turnTime) {
+        secondsElapsed++;
+        notifyListeners();
+      } else {
+        _timer?.cancel();
+      }
+    });
+    notifyListeners();
+  }
+
+  void resetTimer() {
+    _timer?.cancel();
+    secondsElapsed = 0;
+    notifyListeners();
   }
 }
 
@@ -54,27 +93,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
         // TRY THIS: Try changing the color here to a specific color (to
@@ -85,38 +105,45 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      body: Center(child: TimerPage()),
+    );
+  }
+}
+
+class TimerPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final appState = context.watch<MyAppState>();
+    final minutes = (appState.secondsElapsed ~/ 60).toString().padLeft(2, '0');
+    final seconds = (appState.secondsElapsed % 60).toString().padLeft(2, '0');
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            appState.playersName[appState.current - 1],
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Theme.of(context).colorScheme.secondary,
+              foregroundColor: Colors.white,
+              textStyle: Theme.of(context).textTheme.labelLarge
+                  ?.copyWith(fontSize: 48)
+                  .copyWith(fontWeight: FontWeight.bold),
+              minimumSize: Size(250, 400), // Makes the button big and square
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20), // Square corners
+              ),
             ),
-          ],
-        ),
+            onPressed: () {
+              context.read<MyAppState>().nextPlayer();
+            },
+            child: Text('$minutes:$seconds'),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
